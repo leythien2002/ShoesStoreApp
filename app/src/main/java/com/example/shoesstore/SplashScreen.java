f50fd1da3ea0f8435c368275439448701686bead
Le Y Thien <leythien2002@gmail.com>
2023-03-10 15:24:30 +0700

    Design Signin/Signout

---
+++ app/src/main/java/com/example/shoesstore/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.shoesstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Handler h=new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreen.this,Signin.class);
+                startActivity(i);
+            }
+        },3000);
+
+    }
+}
