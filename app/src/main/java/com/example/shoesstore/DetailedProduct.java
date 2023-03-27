f19eb9036b6bb4395a5cc8451b3fdad4a88c3785
Le Y Thien <leythien2002@gmail.com>
2023-03-27 20:21:38 +0700

    Form detail product

---
+++ app/src/main/java/com/example/shoesstore/DetailedProduct.java
@@ -0,0 +1,14 @@
+package com.example.shoesstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_product);
+    }
+}
