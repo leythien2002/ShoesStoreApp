7f3ced528ca08d1070e3c4e4f2a3c45ce5c31123
unknown <leythien2002@gmail.com>
2023-03-29 11:21:40 +0700

    Add to Cart

--- app/src/main/java/com/example/shoesstore/DetailedProduct.java
+++ app/src/main/java/com/example/shoesstore/DetailedProduct.java
@@ -1,17 +1,29 @@
 package com.example.shoesstore;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.shoesstore.models.Product;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.logging.SimpleFormatter;
 
 public class DetailedProduct extends AppCompatActivity {
     ImageView imgDetail,imgPlus,imgMinus;
@@ -26,6 +38,7 @@ public class DetailedProduct extends AppCompatActivity {
         setContentView(R.layout.activity_detailed_product);
 
         initUI();
+        initListener();
         showDetailProduct();
 
     }
@@ -42,6 +55,44 @@ public class DetailedProduct extends AppCompatActivity {
         imgMinus=findViewById(R.id.imgMinus);
 
     }
+    private void initListener(){
+        btnAddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addToCart();
+            }
+        });
+    }
+
+    private void addToCart() {
+        String saveCurrentTime,saveCurrentDate;
+        Calendar calendar= Calendar.getInstance();
+        SimpleDateFormat currentDate=new SimpleDateFormat("MM dd, yyyy");
+        saveCurrentDate=currentDate.format(calendar.getTime());
+        SimpleDateFormat currentTime=new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime=currentTime.format(calendar.getTime());
+
+        HashMap<String,Object> cartMap=new HashMap<>();
+
+        cartMap.put("productName",name.getText().toString());
+        cartMap.put("productPrice",price.getText().toString());
+        cartMap.put("currentDate",saveCurrentDate);
+        cartMap.put("currentTime",saveCurrentTime);
+        FirebaseUser mAuth=FirebaseAuth.getInstance().getCurrentUser();
+        String id=mAuth.getUid();
+        DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference("Cart/"+id);
+        databaseReference.setValue(cartMap, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(DetailedProduct.this,"Add Completed",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+    }
+
     private void showDetailProduct(){
         Object obj=getIntent().getSerializableExtra("detail");
         if(obj instanceof Product){
